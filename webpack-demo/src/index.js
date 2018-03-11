589c1957b29cb701218cbbf845e7b70cc78af454
Tian <skyjunechentian@sina.com>
2018-03-12 07:23:28 +0800

    HtmlWebpackPlugin

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -2,10 +2,12 @@ import _ from 'lodash';
 import './style.css';
 import ImageSrc from './image.jpg';
 import Data from './data.xml';
+import printMe from './print.js'
 
 function component() {
     var element = document.createElement('div');
-   
+    var btn = document.createElement('button');
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
@@ -16,6 +18,11 @@ function component() {
     element.appendChild(myImage)
 
     console.log(Data);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
