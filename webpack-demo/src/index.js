b8ca376460d3628dadfc99c351210f18f72a37dc
Tian <skyjunechentian@sina.com>
2018-03-11 19:08:16 +0800

    loading images

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import Image from './image.jpg';
 
 function component() {
     var element = document.createElement('div');
    
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // Add the image to our existing div
+    var myImage = new Image();
+    myImage.src = Image;
+
+    element.appendChild(myImage)
 
     return element;
 }
