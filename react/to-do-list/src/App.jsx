69f755a5c3f6e03ec9052a0b18ab2da33fded23c
I323917 <tina.chen03@sap.com>
2018-05-18 16:02:48 +0800

    event

--- react/to-do-list/src/App.jsx
+++ react/to-do-list/src/App.jsx
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React , { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class Footer extends Component {
   render() {
     return (
-      <footer>
-        <span><strong></strong> items left</span>
-        <ul class="filters">
+      <footer className="footer">
+        <span><strong>{this.props.count}</strong> items left</span>
+        <ul className="filters">
           <li><a href="">All</a></li>
           <li><a href="">Active</a></li>
           <li><a href="">Completed</a></li>
         </ul>
       </footer>
+    );
+  }
+}
+
+class TodoItem extends Component {
+  render() {
+    const todo = this.props.todo;
+
+    return ( 
+      <li className={todo.completed ? 'completed' : ''}>
+        <div>
+          <input 
+            type="checkbox" 
+            checked={todo.completed}
+            onChange= 
+          />
+          <label htmlFor="">{todo.title}</label>
+          <button>delete</button>
+        </div>
+      
+      </li>
     );
   }
 }
@@ -30,16 +51,50 @@ class Header extends Component {
 
 class App extends Component {
   render() {
+    const rows = [];
+    const todos = this.props.todos;
+    const initialValue = 0;
+    const activeTodoCount = todos.reduce((accumulator, currentValue) => {
+      // accumulator的初始值可以指定为initialValue, 如果没有指定，默认就是Array[0]
+      if(currentValue.completed) {
+        accumulator++;
+      }
+
+      return accumulator;
+    }, initialValue);
+    const completedCount = todos.length - activeTodoCount;
+
+    // var activeTodoCount = todos.reduce(function (accum, todo) {
+    //   return todo.completed ? accum : accum + 1;
+    // }, 0);
+
+    todos.forEach((todo) => {
+      rows.push(
+        <TodoItem key={todo.id} todo={todo}/>
+      );
+
+    });
+
     return (
       <div className="App">
         <Header style={{backGround:'#000'}}/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Footer />
+        <ul>{rows}</ul>
+        <Footer count={activeTodoCount}/>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  todos: PropTypes.object
+};
+
+Footer.propTypes = {
+  count: PropTypes.number
+};
+
+TodoItem.propTypes = {
+  todo: PropTypes.object
+};
 
 export default App;
--- react/to-do-list/src/index.js
+++ react/to-do-list/src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const TODOS = [
+  {id: 'b2d18a3b-0981-45c8-8083-f7aceed0c5e0', title: 'Sporting Goods', completed: true},
+  {id: 'b2d18a3b-0981-45c8-8083-f7aceed0c5e1', title: ' Goods', completed: true},
+  {id: 'b2d18a3b-0981-45c8-8083-f7aceed0c5e2', title: 'Sporg Goods', completed: false}
+];
+
+ReactDOM.render(<App todos={TODOS}/>, document.getElementById('root'));
 registerServiceWorker();
