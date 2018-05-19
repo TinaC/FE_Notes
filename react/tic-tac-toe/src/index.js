56db77bd380fc0610818bacd63cf3b0d5a5bf529
Tian <skyjunechentian@sina.com>
2018-05-19 08:37:13 +0800

    tic

--- react/tic-tac-toe/src/index.js
+++ react/tic-tac-toe/src/index.js
@@ -4,32 +4,15 @@ import './index.css';
 
 // Square is a React component class, or React component type.
 // A component takes in parameters, called props, and returns a hierarchy of views to display via the render method.
-class Square extends React.Component {
-    // state都挪到parent去了
-    // constructor(props) {
-    //     // es6 class的要求
-    //     super(props);
-    //     // this.state should be considered private to the component. 
-    //     // 这时state是属于每个Square的
-    //     // component state is considered private, we can’t update Board’s state directly from Square.
-    //     // 不能通过Square直接更新Board的state
-    //     this.state = {
-    //         value: null,
-    //     }
-    // }
-
-    // The render method returns a description of what you want to render, and then React takes that description and renders it to the screen.
-    // render returns a React element
-    render() {
+function Square(props) { 
       return (
         // 这里用的html, 也可以用JSX
         // 传递给onClick prop 的是一个函数，如果是onClick={alert('click')} 会立即执行，为啥？onClick又没被触发？  onClick={() => alert('click')} 是对的
         // this.setState({value: 'X'} 被替换，使用parent的props function 
-        <button className="square" onClick={() => this.props.onClick()}>      
-          {this.props.value}
+        <button className="square" onClick={props.onClick}>      
+          {props.value}
         </button>
       );
-    }
   }
   
   // the Board component can tell each Square what to display, like how we made each square display its index earlier. 由parent component来控制state, 便于保证children状态同步
@@ -39,6 +22,12 @@ class Square extends React.Component {
       this.state = {
         square: Array(9).fill(null),
       }
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
     }
 
     renderSquare(i) {
