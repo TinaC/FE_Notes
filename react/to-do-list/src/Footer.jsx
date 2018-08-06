854a99bc1e54b48a4ca6942c83defcefd58cb54d
Tina Chen <tian.chen.void@gmail.com>
2018-08-06 10:36:27 +0800

    begin router

--- react/to-do-list/src/Footer.jsx
+++ react/to-do-list/src/Footer.jsx
@@ -7,9 +7,10 @@ class Footer extends Component {
       <footer className="footer">
         <span><strong>{this.props.count}</strong> items left</span>
         <ul className="filters">
-          <li><a href="" onChange={this.props.onSelectAll}>All</a></li>
-          <li><a href="">Active</a></li>
-          <li><a href="">Completed</a></li>
+          <li><a href="#/" onChange={this.props.onSelectAll}>All</a></li>
+          <li><a href="#/active">Active</a></li>
+          <li><a href="#/completed">Completed</a></li>
+          <li><a href="">Clear complete</a></li>
         </ul>
       </footer>
     );
