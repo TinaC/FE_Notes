8265bb583f548fbf201e21165a2b0431d9f13e65
Tina Chen <tian.chen.void@gmail.com>
2018-12-11 16:17:34 +0800

    include reach router

--- react/complete_intro_to_react_v4/src/App.js
+++ react/complete_intro_to_react_v4/src/App.js
@@ -1,72 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
-import pf from "petfinder-client";
-
-// under imports
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import { Router } from "@reach/router";
+import Results from "./Results";
+import Details from "./Detaills";
 
 class App extends React.Component {
-  // inside class, at top
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pets: []
-    };
-  }
-
-  // inside class, above render
-  componentDidMount() {
-    petfinder.pet
-      .find({ location: "Seattle, WA", output: "full" })
-      .then(data => {
-        let pets;
-        // if exist
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            // multiple pet
-            pets = data.petfinder.pets.pet;
-          } else {
-            // just one animal
-            pets = [data.petfinder.pets.pet];
-          }
-        } else {
-          pets = [];
-        }
-        this.setState({
-          pets
-        });
-      });
-  }
-
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        {this.state.pets.map(pet => {
-          let breed;
 
-          if (Array.isArray(pet.breeds.breed)) {
-            breed = pet.breeds.breed.join(".");
-          } else {
-            breed = pet.breeds.breed;
-          }
-
-          return (
-            <Pet
-              key={pet.id}
-              animal={pet.animal}
-              name={pet.name}
-              breed={breed}
-              media={pet.media}
-              location={`${pet.contact.city}, ${pet.contact.state}`}
-            />
-          );
-        })}
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
