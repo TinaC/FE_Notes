bfb23b561c66c3990c6dfdaddbcf2128c68c9c3d
Tina Chen <tian.chen.void@gmail.com>
2018-12-17 09:45:14 +0800

    react context

---
+++ react/complete_intro_to_react_v4/src/SearchContext.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// createContext returns an object with two React components in it: a Provider and a Consumer.
+const SearchContext = React.createContext({
+  location: "Seattle, WA",
+  animal: "",
+  breed: "",
+  breeds: [],
+  handleAnimalChange() {},
+  handleBreedChange() {},
+  handleLocationChange() {},
+  getBreeds() {}
+});
+
+export const Provider = SearchContext.Provider;
+export const Consumer = SearchContext.Consumer;
--- react/complete_intro_to_react_v4/src/SearchParams.js
+++ react/complete_intro_to_react_v4/src/SearchParams.js
@@ -1,115 +1,18 @@
 import React from "react";
-import pf, { ANIMALS } from "petfinder-client";
+import { navigate } from "@reach/router";
+import SearchBox from "./SearchBox";
 
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
-
-class SerachParams extends React.Component {
-  state = {
-    location: "Seattle, WA",
-    animal: "",
-    breed: "",
-    breeds: []
-  };
-  // handle: event 参数
-  // location: state
-  // change: event type
-  handleLocationChange = event => {
-    this.setState({
-      location: event.target.value
-    });
-  };
-  handleAnimalChange = event => {
-    this.setState(
-      {
-        animal: event.target.value,
-        breed: ""
-      },
-      this.getBreeds
-    );
-  };
-  handleBreedChange = event => {
-    this.setState({
-      breed: event.target.value
-    });
-  };
-  getBreeds() {
-    if (this.state.animal) {
-      petfinder.breed
-        .list({ animal: this.state.animal })
-        .then(data => {
-          if (
-            data.petfinder &&
-            data.petfinder.breeds &&
-            Array.isArray(data.petfinder.breeds.breed)
-          ) {
-            this.setState({
-              breeds: data.petfinder.breeds.breed
-            });
-          } else {
-            this.setState({ breeds: [] });
-          }
-        })
-        .catch(console.error);
-    } else {
-      this.setState({
-        breeds: []
-      });
-    }
+class Search extends React.Component {
+  search() {
+    navigate("/");
   }
   render() {
     return (
-      <div className="search-params">
-        <label htmlFor="location">
-          Location
-          <input
-            onChange={this.handleLocationChange}
-            id="location"
-            value={this.state.location}
-            placeholder="Location"
-          />
-        </label>
-
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={this.state.animal}
-            onChange={this.handleAnimalChange}
-            onBlur={this.handleAnimalChange}
-          >
-            <option key="all" value="All animals">All animals</option>
-            {ANIMALS.map(animal => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <label htmlFor="breed">
-          Breed
-          <select
-            disabled={!this.state.breeds.length}
-            id="breed"
-            value={this.state.breed}
-            onChange={this.handleBreedChange}
-            onBlur={this.handleBreedChange}
-          >
-            <option />
-            {this.state.breeds.map(breed => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
-        <button>Submit</button>
+      <div className="search-route">
+        <SearchBox search={this.search} />
       </div>
     );
   }
 }
 
-export default SerachParams;
+export default Search;
